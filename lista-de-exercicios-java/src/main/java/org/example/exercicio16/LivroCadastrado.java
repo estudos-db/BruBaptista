8511dab22bd27c8b2f179c1cb1fb6e2419ccdd64
BrunoBaptista <brunop8129@gmail.com>
2023-03-27 10:04:58 -0300

    Removido a questão 16 da branch, apenas a 19 agora

--- lista-de-exercicios-java/src/main/java/org/example/exercicio16/LivroCadastrado.java
+++
@@ -1,8 +0,0 @@
-package org.example.exercicio16;
-
-public interface LivroCadastrado {
-    boolean isDisponivel();
-    boolean isFisico();
-    void setDisponivel(boolean b);
-    String getTitulo();
-}
