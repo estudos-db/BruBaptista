fc08b386561c04b1dc6fa59843c897cdada7c8ff
BrunoBaptista <brunop8129@gmail.com>
2023-04-05 11:41:07 -0300

    exercicio 8 completo

---
+++ lista-de-exercicios-java/src/main/java/org/example/exercicio08/Loop.java
@@ -0,0 +1,28 @@
+package org.example.exercicio08;
+import java.util.Scanner;
+
+public class Loop {
+    private int resposta;
+    private Scanner scanner;
+
+    public Loop() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void loopar(int numero) {
+        int contagem = 0;
+        while (this.resposta != numero) {
+            System.out.println("Contador: " + contagem++);
+            while (!scanner.hasNextInt()) {
+                System.out.println("Digite um número inteiro válido: ");
+                scanner.next();
+            }
+            int outraResposta = scanner.nextInt();
+            this.resposta = outraResposta;
+        }
+    }
+
+    public int getResposta() {
+        return resposta;
+    }
+}
---
+++ lista-de-exercicios-java/src/main/java/org/example/exercicio08/Main.java
@@ -0,0 +1,8 @@
+package org.example.exercicio08;
+
+public class Main {
+    public static void main(String[] args) {
+        Loop loop = new Loop();
+        loop.loopar(10);
+    }
+}
