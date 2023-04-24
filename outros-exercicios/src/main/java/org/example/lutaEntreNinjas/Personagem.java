d1affc618eb85fca74d233e796848a11712312ac
BrunoBaptista <brunop8129@gmail.com>
2023-04-24 11:18:04 -0300

    personagem + teste

---
+++ outros-exercicios/src/main/java/org/example/lutaEntreNinjas/Personagem.java
@@ -0,0 +1,33 @@
+package org.example.lutaEntreNinjas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Personagem {
+    private String nome;
+    private Map<String, Integer> jutsus;
+    private int chakra = 100;
+    private int vida = 100;
+
+    public Personagem(String nome) {
+        this.nome = nome;
+        this.jutsus = new HashMap<>();
+    }
+
+    public String aprenderJutsu(String jutsu, int dano) {
+        if(!jutsus.containsKey(jutsu)) {
+            jutsus.put(jutsu, dano);
+            StringBuilder sb = new StringBuilder();
+            sb.append("O ninja ")
+                    .append(this.nome)
+                    .append(" aprendeu o jutsu: ")
+                    .append(jutsu);
+            return sb.toString();
+        } else
+            return "Não foi possível aprender esse jutsu";
+    }
+
+    public Map<String, Integer> getJutsus() {
+        return jutsus;
+    }
+}
---
+++ outros-exercicios/src/test/java/org/example/lutaEntreNinjas/PersonagemTest.java
@@ -0,0 +1,40 @@
+package org.example.lutaEntreNinjas;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonagemTest {
+
+    Personagem hina = new Personagem("Hina");
+    @DisplayName("Deve adicionar um novo jutsu")
+    @Test
+    void deveAdicionarJutsu() {
+        assertEquals("O ninja Hina aprendeu o jutsu: rasengan",
+                hina.aprenderJutsu("rasengan", 50));
+        assertTrue(hina.getJutsus().containsKey("rasengan"));
+    }
+
+    @DisplayName("Não deve adicionar um jutsu repetido")
+    @Test
+    void naoDeveAdicionarJutsu() {
+        hina.aprenderJutsu("rasengan", 50);
+
+        assertEquals("Não foi possível aprender esse jutsu",
+                hina.aprenderJutsu("rasengan", 50));
+    }
+
+    @DisplayName("Deve retornar os jutsus que o personagem sabe")
+    @Test
+    void deveRetornarJutsus() {
+        hina.aprenderJutsu("rasengan", 50);
+        Map<String, Integer> jutsusEsperados = new HashMap<>();
+        jutsusEsperados.put("rasengan", 50);
+
+        assertEquals(jutsusEsperados, hina.getJutsus());
+    }
+}
